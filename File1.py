602720f882293e64b7da84d7eeb4b46e0452db78
vikaskhullar <vikas.khullar@gmail.com>
2020-11-05 21:07:01 +0530

    Basics

---
+++ File1.py
@@ -0,0 +1,94 @@
+# Welcome to CBAP
+
+'''
+Welcome to CBAP
+Welcome to CBAP
+Welcome to CBAP
+'''
+
+# F5 to run full file
+# F9 to run selected line of code
+
+
+print?
+
+print ('Welcome to CBAP')
+print ('Welcome','to CBAP')
+print ('Welcome','to CBAP', sep =',')
+
+
+print ('Welcome to CBAP', end='******')
+print ('Welcome','to CBAP', end='*****')
+print ('Welcome','to CBAP', sep =',', end='    ')
+
+help(print)
+
+help(numpy)
+
+import numpy
+
+help(numpy)
+
+!pip install numpy
+
+import numpy
+numpy.__version__
+
+
+!pip install --upgrade pip
+
+!conda list
+
+!conda search scipy
+
+
+
+#Data Type
+#integer int
+#floating float
+#string combination of charachters
+#boolean True/False   1/0
+
+
+a = input("Enter your name - > ")
+print (a)
+
+b= 'Khullar'
+
+type(a)
+type(b)
+
+
+a = 10
+b = 20
+
+type (a)
+type (b)
+
+
+a = 10.4
+b = 100.333
+
+type(a)
+type(b)
+
+a = True
+b = False
+
+type(a)
+type(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
